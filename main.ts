f09ab0714623c01b54145626ba65c460178b5897
Kiki Prottsman <kiki-lee@users.noreply.github.com>
2021-09-30 13:29:07 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+{
+    "name": "Save the Forest Assets Extension",
+    "description": "",
+    "dependencies": {
+        "device": "*"
+    },
+    "files": [
+        "assets.json",
+        "tilemap.g.jres",
+        "tilemap.g.ts",
+        "images.g.jres",
+        "images.g.ts"
+    ],
+    "targetVersions": {
+        "branch": "v1.5.46",
+        "tag": "v1.5.46",
+        "commits": "https://github.com/microsoft/pxt-arcade/commits/4f3f29bef862bcab766a47e42db2d3ed6b0060b1",
+        "target": "1.5.46",
+        "pxt": "7.1.25"
+    },
+    "preferredEditor": "tsprj"
+}
